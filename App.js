b537810d29677ddd2381dfb92a3cd578bbac2551
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-11-05 22:22:31 -0300

    finish the first layout of APP

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
+// import logo from './assets/ip2.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+         <Text style={styles.made} >Olá dev, você está no React Native!</Text>
+         {/* <Image source={logo}/> */}
+        </View>
+      
+      <View style={styles.body}>
+        
+     
+        <Text style={styles.ip}>IP</Text>
+        <Button title="Decobrir meu IP!"/>
+      </View>
+      <View style={styles.footer}>
+       <Text style={styles.made}>Feio com ❤ por Honorato!</Text>
+      </View>
+     
     </View>
   );
 }
@@ -14,8 +28,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#2f2336',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
+  body: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+ },
+ ip:{
+  color: 'white',
+  paddingBottom: 20,
+  paddingTop:20
+ },
+ header:{
+  paddingTop:30,
+  paddingBottom:10
+ },
+ footer: {
+  paddingTop:10,
+  paddingBottom:10
+ },
+ made: {
+  color:'white',
+  textAlign:'center'
+ }
 });
